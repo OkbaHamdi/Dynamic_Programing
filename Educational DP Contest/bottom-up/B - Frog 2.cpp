7254f6bf9ad3f0da90c4258cc72736f36058b1c5
Okba HAMDI <94719238+OkbaHamdi@users.noreply.github.com>
2023-10-04 10:56:24 +0100

    Create B - Frog 2.cpp

---
+++ Educational DP Contest/bottom-up/B - Frog 2.cpp
@@ -0,0 +1,60 @@
+/**                         _       _______
+                           | |     |____   /
+                           | |          / /
+                           | |         / /
+                           | |____    / /
+                           |______|  /_/
+**/
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef         long long ll;
+typedef         long double ld;
+typedef         vector<ll>vl;
+typedef         deque<ll>dq;
+typedef         pair<ll,ll>pl;
+#define         ordered_set tree<int, null_type,less<int>, rb_tree_tag,tre
+#define         yes {cout<<"YES"<<endl;return;}
+#define         no {cout<<"NO"<<endl;return;}
+#define         CR(_) {cout<<_<<endl;return;}
+#define         F first
+#define         S second
+#define         pb push_back
+#define         ppb pop_back
+#define         pf push_front
+#define         ppf pop_front
+#define         aff(v) for(auto e:v) cout<<e<<" ";cout<<endl;
+#define         all(v) v.begin(), v.end()
+#define         rall(v) v.rbegin(), v.rend()
+//const           ll MOD = LLONG_MAX;
+//const           int MOD = 998244353;
+const           int MOD=1000000007;
+const           ld eps=1e-7;
+const           double PI=acos(-1);
+const           int N=2e5+5;
+const           ll INF=1e18;
+const           int L = log2(N)+1;
+void solve(){
+    int n,k;cin>>n>>k;
+    vector<ll>v(n+1);
+    for(ll i=1;i<=n;i++)cin>>v[i];
+    vector<ll>dp(n+1,0);
+    for(int i=2;i<=n;i++){
+        dp[i]=INF;
+        for(int j=1;j<=min(i-1,k);j++)dp[i]=min(dp[i-j]+abs(v[i-j]-v[i]),dp[i]);
+    }
+    cout<<dp[n]<<endl;
+
+}
+int main(){
+        ios_base::sync_with_stdio(false);cin.tie(0);
+        //freopen("input.txt", "r",stdin);
+        //freopen("output.txt", "w",stdout);
+        int _=1;
+        //cin >>_;
+        //cout<<fixed<<setprecision(6);
+        while(_--)solve();
+}
